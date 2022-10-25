2c9e61cf930bb55c5f473d3c266ab4a1ac45455b
sorrh <desorrh@gmail.com>
2022-10-26 01:48:42 +0900

    wsgi.py 수정

--- mlproject/mlproject/wsgi.py
+++ mlproject/mlproject/wsgi.py
@@ -7,9 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('./myvenv/lib/site-packages')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mlproject.settings')
 
